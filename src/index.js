40280916cd2c84904d4be89f7a76ae65af2103e2
Nikita622T <89455946+Nikita622T@users.noreply.github.com>
2023-08-08 12:39:53 +0200

    de

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import * as ReactDOMClient from "react-dom/client"
-import App from './App'
-
-const app = ReactDOMClient.createRoot(document.getElementById("app"))//находим div с тегом app и помещаем в переменную
-//новый способ для вывода информации
-app.render(<App />) //помещаем во внутрь тега div компонент App
